0ae0116e079fe8abef5b0500e62cea499e9bc50c
Ankush <ankushransiwal.13@gmail.com>
2018-07-16 13:11:05 +0530

    Organise the code into methods and classes

--- app/src/main/java/com/example/ankush/darkskyclient/MainActivity.java
+++ app/src/main/java/com/example/ankush/darkskyclient/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import com.example.ankush.darkskyclient.models.Currently;
 import com.example.ankush.darkskyclient.models.Weather;
 import com.example.ankush.darkskyclient.services.WeatherService;
+import com.example.ankush.darkskyclient.services.WeatherServiceProvider;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -23,26 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://api.darksky.net/forecast/d7f22eb84e853202ead966b3aae24820/19.0760,72.8777/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        WeatherService weatherService = retrofit.create(WeatherService.class);
-        Call<Weather> weatherData = weatherService.getWeather();
-        weatherData.enqueue(new Callback<Weather>() {
-            @Override
-            public void onResponse(Call<Weather> call, Response<Weather> response) {
-                Currently currently = response.body().getCurrently();
-                Log.e(TAG,"Temparature : " + currently.getTemperature());
-            }
-
-            @Override
-            public void onFailure(Call<Weather> call, Throwable t) {
-                Log.e(TAG,"onFailure : unable to get Weather Data");
-
-            }
-        });
-
+        WeatherServiceProvider weatherServiceProvider = new WeatherServiceProvider();
+        weatherServiceProvider.getWeather();
     }
 }
--- app/src/main/java/com/example/ankush/darkskyclient/services/WeatherServiceProvider.java
+++ app/src/main/java/com/example/ankush/darkskyclient/services/WeatherServiceProvider.java
@@ -1,4 +1,50 @@
 package com.example.ankush.darkskyclient.services;
 
+import android.util.Log;
+
+import com.example.ankush.darkskyclient.models.Currently;
+import com.example.ankush.darkskyclient.models.Weather;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class WeatherServiceProvider {
+
+    private static final String BASE_URL = "https://api.darksky.net/forecast/d7f22eb84e853202ead966b3aae24820/19.0760,72.8777/";
+    private static final String TAG = WeatherServiceProvider.class.getSimpleName();
+    private Retrofit retrofit;
+
+
+    Retrofit getRetrofit(){
+        if(this.retrofit == null){
+            this.retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return this.retrofit;
+
+    }
+
+    public void getWeather(){
+        WeatherService weatherService = getRetrofit().create(WeatherService.class);
+        Call<Weather> weatherData = weatherService.getWeather();
+        weatherData.enqueue(new Callback<Weather>() {
+            @Override
+            public void onResponse(Call<Weather> call, Response<Weather> response) {
+                Currently currently = response.body().getCurrently();
+                Log.e(TAG,"Temparature : " + currently.getTemperature());
+            }
+
+            @Override
+            public void onFailure(Call<Weather> call, Throwable t) {
+                Log.e(TAG,"onFailure : unable to get Weather Data");
+
+            }
+        });
+    }
+
 }
